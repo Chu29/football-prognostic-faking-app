034dd9ba7ced1b08f64b912b507f27b78cda36bc
Chu A. <chu.amk22@gmail.com>
2025-12-08 11:30:52 +0100

    feat(ui): Implement Team component

---
+++ src/components/Team.jsx
@@ -0,0 +1,15 @@
+import { FaPlus } from "react-icons/fa6";
+import './Team.css'
+
+const Team = ({description}) => {
+  return (
+    <div className="select-team-ctn">
+      <div className="select-team">
+        <FaPlus className="add-team" />
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Team;
