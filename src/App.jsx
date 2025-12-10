919ddc94a2b295193c594e2bc8c7832b27fd5f49
Chu A. <chu.amk22@gmail.com>
2025-12-10 12:26:37 +0100

    feat: Implement functions 'handleScoreChange', 'handleHideModal', 'handleShowModal' and 'handleTeamSelect'

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,83 @@ import "./App.css";
 import Button from "./components/Button";
 import { RxDownload } from "react-icons/rx";
 import Modal from "./components/Modal/Modal";
+import { useState } from "react";
+
+const initialState = {
+  name: "TEAM",
+  score: 0,
+  flag: "placeholder.jpg",
+};
 
 const App = () => {
+  const [homeTeam, setHomeTeam] = useState(initialState);
+  const [awayTeam, setAwayTeam] = useState(initialState);
+  const [showModal, setShowModal] = useState(false);
+  const [teamToSelect, setTeamToSelect] = useState(null);
+
+  const handleScoreChange = (teamKey, action) => {
+    const setter = teamKey === "home" ? setHomeTeam : setAwayTeam;
+    setter((prev) => {
+      let newScore = prev.score;
+      if (action === "increment") {
+        newScore += 1;
+      } else if (action === "decrement" && newScore > 0) {
+        newScore -= 1;
+      }
+      return { ...prev, score: newScore };
+    });
+  };
+
+  const handleShowModal = (teamKey) => {
+    setTeamToSelect(teamKey);
+    setShowModal(true);
+  };
+
+  const handleHideModal = () => {
+    setShowModal(false);
+  };
+
+  const handleTeamSelect = (selectedTeamData) => {
+    const setter = teamToSelect === "home" ? setHomeTeam : setAwayTeam;
+    setter((prev) => ({
+      ...prev,
+      name: selectedTeamData.name,
+      flag: selectedTeamData.flag,
+    }));
+  };
+
   return (
     <>
       <h1>Football Prognostiker</h1>
       <main>
-        <Team description="Select Home Team" />
+        <Team handleShowModal={() => handleShowModal("home")} team={homeTeam} />
         <div className="scores">
-          <Score />
+          <Score
+            score={homeTeam.score}
+            onIncrement={() => handleScoreChange("home", "increment")}
+            onDecrement={() => handleScoreChange("home", "decrement")}
+          />
           <span>-</span>
-          <Score />
+          <Score
+            score={awayTeam.score}
+            onIncrement={() => handleScoreChange("away", "increment")}
+            onDecrement={() => handleScoreChange("away", "decrement")}
+          />
         </div>
-        <Team description="Select Away Team" />
+        <Team handleShowModal={() => handleShowModal("away")} team={awayTeam} />
       </main>
       <div className="download-btn">
         <RxDownload />
         <Button />
       </div>
-      <Modal />
+      <div>
+        {showModal && (
+          <Modal
+            handleHideModal={handleHideModal}
+            onSelect={handleTeamSelect}
+          />
+        )}
+      </div>
     </>
   );
 };
