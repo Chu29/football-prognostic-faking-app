d453fe1cf527a6fe8f98ca941c9377aa4af56cec
Chu A. <chu.amk22@gmail.com>
2025-12-10 12:25:01 +0100

    feat: Implement functions 'handleTeamClick', 'handleSelectionConfirm'

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,48 +1,76 @@
 import { CgClose } from "react-icons/cg";
 import data from "../../meta/football.json";
 import "./Modal.css";
+import { useState } from "react";
 
-const Modal = () => {
+const Modal = ({ handleHideModal, onSelect }) => {
   const { clubs, countries } = data;
-  // const [filter, setFilter] = useState(true);
+  const [selectedTeam, setSelectedTeam] = useState(null);
 
-  const entries = Object.entries(data);
-  console.log(entries);
+  const handleTeamClick = (team) => {
+    setSelectedTeam(team);
+  };
+
+  const handleSelectConfirm = () => {
+    if (selectedTeam) {
+      onSelect({
+        name: selectedTeam.name.toUpperCase(),
+        flag: selectedTeam.flag,
+      });
+      handleHideModal();
+    }
+  };
+
+  const allTeams = [
+    ...clubs
+      .filter((club) => (club.url !== null ? club.url : ""))
+      .map((club) => ({
+        name: club.name,
+        flag: club.url,
+        types: "club",
+      })),
+    ...countries
+      .filter((country) => (country.flag !== null ? country.flag : ""))
+      .map((country) => ({
+        name: country.country,
+        flag: country.flag,
+        type: "country",
+      })),
+  ];
 
   return (
-    <div className="modal-ctn">
-      <h3 className="modal-header">
-        Select Team <CgClose className="close-btn" />
-      </h3>
-      <div className="team-cat">
-        <div tabIndex={0}>All Teams</div>
-        <div tabIndex={0}>National Teams</div>
-        <div tabIndex={0}>Club Teams</div>
+    <>
+      <div className="modal-overlay"></div>
+      <div className="modal-ctn">
+        <h3 className="modal-header">
+          Select Team{" "}
+          <CgClose onClick={handleHideModal} className="close-btn" />
+        </h3>
+
+        <div className="flags-ctn">
+          {allTeams.map((team, index) => (
+            <div
+              key={index}
+              className="flags"
+              onClick={() => handleTeamClick(team)}
+            >
+              <img key={index} src={team.flag} alt="Flag" loading="lazy" />
+              <span>{team.name}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="btn-ctn">
+          <button
+            onClick={handleSelectConfirm}
+            disabled={!selectedTeam}
+            className="confirm-select-btn"
+          >
+            Confirm Selection
+          </button>
+        </div>
       </div>
-      <div className="flags-ctn">
-        {clubs.map(
-          (club, index) =>
-            (
-              <div key={index} className="flags">
-                <img key={index} src={club.url} alt="Flag" loading="lazy" />
-                <span>{club.name}</span>
-              </div>
-            ) ||
-            countries.map()((country, index) => (
-              <div key={index} className="flags">
-                <img key={index} src={country.url} alt="Flag" loading="lazy" />
-                <span>{country.name}</span>
-              </div>
-            ))
-        )}
-        {/* {countries.map()((country, index) => (
-          <div key={index} className="flags">
-            <img key={index} src={country.url} alt="Flag" loading="lazy" />
-            <span>{country.name}</span>
-          </div>
-        ))} */}
-      </div>
-    </div>
+    </>
   );
 };
 
